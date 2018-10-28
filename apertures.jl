9b9bea9ff62ed481e2cc63cbac2b6a6b2ee3d7f0
Ganesh Ajjanagadde <gajjanag@alum.mit.edu>
2018-10-28 14:52:34 -0400

    apertures: mostly cleaned up
    Comments added, and commands used to generate plots for the paper are given.
    Need to add a tag for the submitted version.

--- apertures.jl
+++ apertures.jl
@@ -1,22 +1,30 @@
-using Random
-using Distributions
-using Printf
-using Optim
-using StatsBase
-using Primes
 using FFTW
-using LinearAlgebra
 using Plots
 using Roots
+using Optim
+using Distributions
+using Random
+using LinearAlgebra
+using ProgressMeter
+using Primes
 
+"""
+Nazarov's ``magic'' Phi function x*arctan(x)-(1/2)log(1+x*x)
+"""
 function nazarov_phi(x::Real)
     return x*atan(x) - 0.5*log(1+x*x)
 end
 
+"""
+Derivative Phi'(x)
+"""
 function nazarov_phiprime(x::Real)
     return atan(x)
 end
 
+"""
+Nazarov's I(f)
+"""
 function nazarov_potential(fv::Array)
     s = 0
     for x in fv
@@ -25,14 +33,90 @@ function nazarov_potential(fv::Array)
     return s
 end
 
+"""
+Real orthonormal basis for the DFT. We only return non-DC terms.
+"""
+function dft_mat(n::Integer)
+    mat = zeros(n,n)
+    q = 2*pi/n
+    h_pt = ceil(Int, (n+1)/2)
+    # ceil((n+1)/2) cosines, floor((n-1)/2) sines
+    for i=1:h_pt
+        for j=1:n
+            mat[i,j] = sqrt(2)*cos(q*(i-1)*(j-1))
+        end
+    end
+    for i=h_pt+1:n
+        for j=1:n
+            mat[i,j] = sqrt(2)*sin(q*(i-h_pt)*(j-1))
+        end
+    end
+    mat[1,:] /= sqrt(2)
+    if iseven(n)
+        mat[h_pt,:] /= sqrt(2)
+    end
+    return mat
+end
+
 function nazarov_psi(n::Integer)
     return dft_mat(n)[2:end,:]
 end
 
+"""
+Compute the Nazarov constant for the DFT. In the paper's notation, this is M(n).
+"""
+function nazarov_const(n::Integer)
+    mat = dft_mat(n)
+    nc = 3*pi/2
+    max_rat = 0
+    for i=1:n
+        nrat = 1/norm(mat[i,:],1)
+        if (nrat > max_rat)
+            max_rat = nrat
+        end
+    end
+    max_rat *= n
+    return nc*max_rat*max_rat
+end
+
+function plot_nazarov_const(n1::Integer, n2::Integer)
+    x = primes(n1,n2)
+    plot(x, [nazarov_const(i) for i in x])
+end
+
+"""
+Some functions for checking the l1 norm beta(n) for the DFT. Asymptotics are given
+in the paper. Finite n behavior can be studied with these routines, basically
+the above nazarov_const is governed by the minimal value of the l1 norm over the
+divisors of n.
+"""
+function l1_cos(n::Integer)
+    sum = 0
+    omega = 2*pi/n
+    for i=0:n-1
+        sum += abs(cos(omega*i))
+    end
+    sum /= n
+    return sum-2/pi
+end
+
+function plot_l1_cos(n1::Integer, n2::Integer)
+    x = n1:n2
+    y = [l1_cos(n) for n in x]
+    p = plot(x,y)
+    savefig(p, "/tmp/l1_dft.pdf")
+end
+
+"""
+Seed the greedy local optimum search randomly, works well in practice.
+"""
 function nazarov_seed_cortege(n::Integer)
     return rand([1,-1], n)
 end
 
+"""
+Perform the local optimum search, given a vector that describes the power control.
+"""
 function nazarov_search!(a::Array, psi::Array, cortege::Array{Int})
     n = length(cortege)
     vn = length(psi[1,:])
@@ -41,10 +125,12 @@ function nazarov_search!(a::Array, psi::Array, cortege::Array{Int})
     cur_pot = nazarov_potential(fv)
     iter = 0
     is_opt = false
-    println("iter: pot")
+    println("-----------------------------------------------------------------")
+    println("Performing local optimum search...")
+    println("(iter, pot)")
     while !is_opt
         iter += 1
-        println("$iter: $(cur_pot/vn)")
+        print("($iter, $(cur_pot/vn))")
         randperm!(iter_order)
         is_opt = true
         for j in iter_order
@@ -59,8 +145,14 @@ function nazarov_search!(a::Array, psi::Array, cortege::Array{Int})
             fv += 2*cortege[j]*a[j]*psi[j,:]
         end
     end
+    println("\n---------------------------------------------------------------")
 end
 
+"""
+Perform the shift/scale to get a [0,1] sequence from the locally optimal cortege.
+We do not do the scaling of the paper, as it is conservative and meant for theorems.
+The scaling here works better in practice and is natural.
+"""
 function _nazarov_aperture(a::Array, psi::Array, cortege::Array{Int})
     n = length(cortege)
     vn = length(psi[1,:])
@@ -91,16 +183,24 @@ function _nazarov_aperture(a::Array, psi::Array, cortege::Array{Int})
     return aper
 end
 
-function nazarov_aperture(n::Integer)
+"""
+General function that hooks things together: just takes in an n, a power control
+vector (not necessarily normalized), and calls the above routines to get an aperture.
+"""
+function nazarov_aperture(n::Integer, powers::Array)
     psi = nazarov_psi(n)
-    a = (1/sqrt(n-1))*ones(n-1)
+    a = powers/norm(powers)
     cortege = nazarov_seed_cortege(n-1)
     nazarov_search!(a, psi, cortege)
+    #redirect_stdout(() -> nazarov_search!(a, psi, cortege), open("/dev/null", "w"))
     return _nazarov_aperture(a, psi, cortege)
 end
 
-function nazarov_ahat(n::Integer, ctx::FFTW.cFFTWPlan)
-    aperture = nazarov_aperture(n)
+"""
+Sanity checks on the generated aperture, and computation of its spectrum
+"""
+function nazarov_ahat(n::Integer, powers::Array, ctx::FFTW.cFFTWPlan)
+    aperture = nazarov_aperture(n, powers)
     @assert(minimum(aperture) >= 0)
     @assert(maximum(aperture) <= 1)
     ahat = ctx * aperture
@@ -113,29 +213,28 @@ function nazarov_ahat(n::Integer, ctx::FFTW.cFFTWPlan)
     return ahat
 end
 
-function nazarov_test(n::Integer)
-    aperture = nazarov_aperture(n)
-    @assert(maximum(aperture) <= 1)
-    @assert(minimum(aperture) >= 0)
-    rho = sum(aperture)/n
-    power = sum([x^2 for x in aperture])/n
-    println("rho: $rho")
-    println("power: $power")
-    ahat = fft(aperture)
-    spec = [abs(x)^2 for x in ahat]
-    min_spec = minimum(spec)/n
-    println("min spec: $min_spec")
-    theory_min = rho*(1-rho)
-    loss_factor = rho*(1-rho)/min_spec
-    println("Nazarov loss: $loss_factor")
-    return aperture
+"""
+Scene correlation functions, generated by sampling at n equally spaced points
+in [0,1] of a nonnegative, bounded, continuous function with symmetry about 1/2.
+"""
+function exp_corr(theta::Real, beta::Real, n::Integer)
+    f = x -> (x < 0.5) ? theta*(beta^x) : theta*(beta^(1-x))
+    d = (1/n)*ones(n)
+    for i=1:n
+        j = i-1
+        d[i] *= f(j/n)
+    end
+    return d
 end
 
 function iid_corr(theta::Real, n::Integer)
     return (theta/n)*ones(n)
 end
 
-# constant factor loss for iid scene
+"""
+Computation of power loss factor, i.e using a suboptimal \rho. Here, a
+(corresponding to W/J) is fixed.
+"""
 function _loss_factor(a::Real)
     num = 2*a - 2*sqrt(a*(a+1))+1
     rhos = [0.25, 0.5]
@@ -144,29 +243,10 @@ function _loss_factor(a::Real)
     return num/denom
 end
 
-function exp_corr(theta::Real, beta::Real, n::Integer)
-    d = (theta/n)*ones(n)
-    h = ceil(Int, (n+1)/2)
-    for i=2:h
-        d[i] *= beta^((i-1)/(h-1))
-        d[n+2-i] = d[i]
-    end
-    return d
-end
 
-function power_corr(theta::Real, beta::Real, gamma::Real, n::Integer)
-    d = (theta/n)*ones(n)
-    h = ceil(Int, (n+1)/2)
-    d[1] *= beta^(-gamma)
-    for i=2:h
-        d[i] *= (beta*i)^(-gamma)
-        d[n+2-i] = d[i]
-    end
-    d /= sum(d)
-    d *= beta
-    return d
-end
-
+"""
+LMMSE expressions.
+"""
 function eval_mmse(d::Array, n::Integer, t::Real, w::Real, j::Real, ahat::Array)
     mmse = 0
     rho = ahat[1]/n
@@ -230,6 +310,32 @@ function frac(x::Real)
     return x-floor(x)
 end
 
+"""
+Waterfilling computation for power control.
+"""
+function waterfill_powers!(powers::Array, d::Array, rho::Real, n::Integer, t::Real, w::Real, j::Real)
+    gamma = t/(n*(w+rho*j))
+    # A hack for t=0
+    if (gamma == 0)
+        for i=1:n-1
+            j = i+1
+            powers[i] = 1/sqrt(n-1)
+        end
+        return
+    end
+    r = n*rho
+    p = n*(floor(r)+frac(r)*frac(r))-r*r
+    ub = (1/d[1])+gamma*p
+    c = find_zero(x -> _mmse_power_sum(x,d,gamma)-p, (0,1.5*ub))
+    for i=1:n-1
+        j = i+1
+        powers[i] = max(0, (c-(1/d[j]))/gamma)
+    end
+end
+
+"""
+Lower bound on LMMSE based on waterfilling.
+"""
 function mmse_lb(d::Array, rho::Real, n::Integer, t::Real, w::Real, j::Real)
     gamma = t/(n*(w+rho*j))
     r = n*rho
@@ -246,39 +352,51 @@ function mmse_lb(d::Array, rho::Real, n::Integer, t::Real, w::Real, j::Real)
     return mmse
 end
 
-function plot_lmmse(n::Integer, theta::Real, w::Real, j::Real, start::Real=0, len::Real=4)
-    d = iid_corr(theta, n)
+"""
+Finally, the function that generates the plots of the paper.
+Set n=251, theta=1, w=j=0.001, start = 0, len=10.
+1. iid: Set beta = 1, and call plot_lmmse(n,theta,beta,w,j,start,len) to get
+a pdf plot at /tmp/mmse_iid.pdf. This is the top plot of the paper.
+2. exp: Set beta = 0.1, and call plot_lmmse(n,theta,beta,w,j,start,len) to get
+a pdf plot at /tmp/mmse_exp.pdf. This is the bottom plot of the paper.
+"""
+function plot_lmmse(n::Integer, theta::Real, beta::Real, w::Real, j::Real, start::Real=0, len::Real=4)
+    d = exp_corr(theta, beta, n)
     fft_ctx = plan_fft(zeros(n))
+    waterfill = zeros(n-1)
     x = range(start, length=256, stop=len)
     y_lb = zeros(length(x))
     popt = zeros(length(x))
     y_prng = zeros(length(x))
     y_spec = zeros(length(x))
     y_nazarov = zeros(length(x))
-    ahat_nazarov = nazarov_ahat(n, fft_ctx)
-    rho_arr = [0.5]
+    rho_arr = [0.25] # depends on the value of n that we use, this is for n=251
     i = 1
-    for t in x
+    @showprogress for t in x
         t *= n
         # lower bound
         res = optimize(rho->mmse_lb(d,rho,n,t,w,j), 0, 1)
         y_lb[i] = 10*log10(Optim.minimum(res))
         popt[i] = Optim.minimizer(res)
+        waterfill_powers!(waterfill,d,popt[i],n,t,w,j)
         # random sequence
         res = optimize(rho->eval_mmse_rand(d,rho,n,t,w,j,fft_ctx),0,1)
         y_prng[i] = 10*log10(Optim.minimum(res))
-        # spectrally flat, rho=1/2,1/4,1/8
+        # spectrally flat, rho in rho_arr
         y_spec[i] = 10*log10(minimum([eval_mmse_flat(d,rho,n,t,w,j) for rho in rho_arr]))
         # Nazarov construction (iid case only, FIXME)
+        ahat_nazarov = nazarov_ahat(n, waterfill, fft_ctx)
         y_nazarov[i] = 10*log10(eval_mmse(d,n,t,w,j,ahat_nazarov))
         i += 1
     end
     p = plot(x, [y_lb, y_prng, y_spec, y_nazarov],
-             title="LMMSE (dB) vs time for iid scene at\n (n,theta,w,j)=($n,$theta,$w,$j)",
-             label=["lower bound", "opt rand p", "spec flat", "Nazarov"],
+             label=["lower bound", "optimal random on-off", "spectrally flat", "Nazarov"],
              xlabel="exposure time (divided by n)",
-             ylabel="LMMSE")
+             ylabel="LMMSE (dB)")
     plot_str = "/tmp/mmse_iid.pdf"
+    if (beta != 1)
+        plot_str = "/tmp/mmse_exp.pdf"
+    end
     savefig(p, plot_str)
     q = plot(x, popt,
              title="optimal p vs time for iid scene at\n (n,theta,w,j)=($n,$theta,$w,$j)",
@@ -287,77 +405,4 @@ function plot_lmmse(n::Integer, theta::Real, w::Real, j::Real, start::Real=0, le
              ylabel="p")
     plot_str = "/tmp/optp_iid.pdf"
     savefig(q, plot_str)
-end
-
-function auto_corr(v::Array)
-    n = length(v)
-    cv = zeros(n)
-    for i=0:n-1
-        for j=0:n-1
-            cv[i+1] += v[j+1]*v[((j+i)%n)+1]
-        end
-    end
-    return cv
-end
-
-function countnz(v::Array)
-    return count(i->i!=0, v)
-end
-
-function nazarov_const(n::Integer)
-    mat = dft_mat(n)
-    nc = 3*pi/2
-    max_rat = 0
-    for i=1:n
-        nrat = 1/norm(mat[i,:],1)
-        if (nrat > max_rat)
-            max_rat = nrat
-        end
-    end
-    max_rat *= n
-    return nc*max_rat*max_rat
-end
-
-function l1_cos(n::Integer)
-    sum = 0
-    omega = 2*pi/n
-    for i=0:n-1
-        sum += abs(cos(omega*i))
-    end
-    sum /= n
-    return sum-2/pi
-end
-
-function plot_l1_cos(n1::Integer, n2::Integer)
-    x = n1:n2
-    y = [l1_cos(n) for n in x]
-    p = plot(x,y)
-    savefig(p, "/tmp/foo.pdf")
-end
-
-function plot_nazarov_const(n1::Integer, n2::Integer)
-    x = primes(n1,n2)
-    plot(x, [nazarov_const(i) for i in x])
-end
-
-function dft_mat(n::Integer)
-    mat = zeros(n,n)
-    q = 2*pi/n
-    h_pt = ceil(Int, (n+1)/2)
-    # ceil((n+1)/2) cosines, floor((n-1)/2) sines
-    for i=1:h_pt
-        for j=1:n
-            mat[i,j] = sqrt(2)*cos(q*(i-1)*(j-1))
-        end
-    end
-    for i=h_pt+1:n
-        for j=1:n
-            mat[i,j] = sqrt(2)*sin(q*(i-h_pt)*(j-1))
-        end
-    end
-    mat[1,:] /= sqrt(2)
-    if iseven(n)
-        mat[h_pt,:] /= sqrt(2)
-    end
-    return mat
 end
